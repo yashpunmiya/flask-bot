3cd43d6f499476ea516659bfb7cce1c0b96f723d
yashpunmiya <yyash7379@gmail.com>
2024-11-13 00:07:27 +0530

    port repair

--- app.py
+++ app.py
@@ -40,5 +40,8 @@ def chat_gemini():
 
     return render_template('chat_bot.html', response=response_text)
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
 if __name__ == "__main__":
     app.run(debug=True)
